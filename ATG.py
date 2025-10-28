2cc7800fbccc56bcfc712309bbbaeabb61747708
GravityGravity <154651849+GravityGravity@users.noreply.github.com>
2025-10-28 02:30:01 -0400

    Added some functions for code prep

--- ATG.py
+++ ATG.py
@@ -1,1 +1,19 @@
 # FILE: ATG.py
+
+
+
+
+# Option 0: Generate data struct
+def gen_struct():
+  
+  return 0
+
+# Option 1: Generate fault collapse
+def fault_coll():
+  return 0
+
+def 
+
+
+while(True):
+  option = input('Enter an option:\n 0: generate data structures\n1: perform fault collapsing and create fault classes\n2:Display found fault classes\n3:Simulate the circuit\n4:PODEM algo\n5:D-Algorithm\n6:Placeholder algo\n7: Reset ATG')
