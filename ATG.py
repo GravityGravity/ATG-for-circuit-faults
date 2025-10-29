d66b6ecef58aba230e46df29bec35327c1f5b6ba
Gravity <isaakhern@outlook.com>
2025-10-29 01:29:11 -0400

    Improved console option functionality + more
        +Added colorama lib for colored terminal output
        +Added file read for .ckt circuit files for option 0.
        +Added an option switch cases for while loop
    
        +Created new requirements.txt file for library requirements

--- ATG.py
+++ ATG.py
@@ -1,19 +1,86 @@
 # FILE: ATG.py
 
+import os
+import colorama as cl
 
-
+# Auto reset colored console print
+cl.init(autoreset=True)
 
 # Option 0: Generate data struct
+
+
 def gen_struct():
-  
-  return 0
+
+    ckt_name = input(
+        '    Please input desired circuit file name\n' + cl.Fore.YELLOW + 'ATG.py>>File Name> ' + cl.Style.RESET_ALL)
+
+    # Create path to benchmarks folder that contains .ckt circuit file
+    ckt_path = os.path.join(os.path.join(
+        os.path.dirname(__file__), 'benchmarks'), (ckt_name))
+
+    print(cl.Fore.CYAN + 'DEBUG:    ' + ckt_path)  # debug
+
+    try:
+        with open(ckt_path, 'r') as ckt_txt:
+            print('    OPENED CKT FILE: ' + cl.Fore.GREEN + ckt_name)  # DEBUG
+    except FileNotFoundError:
+        print(cl.Fore.RED + '    ERROR: "' + ckt_name +
+              '" file not found\n        '
+              '-Ensure your circuit is within the benchmarks folder\n        '
+              '-Ensure the benchmarks folder is in the same directory as this python file\n        '
+              '-Ensure you add [filename].ckt to when prompted for name\n')
+        return 0
+
+    # PARSE HERE
+
+    return 0
 
 # Option 1: Generate fault collapse
+
+
 def fault_coll():
-  return 0
+    return 0
 
-def 
+# Prints console commands
 
 
-while(True):
-  option = input('Enter an option:\n 0: generate data structures\n1: perform fault collapsing and create fault classes\n2:Display found fault classes\n3:Simulate the circuit\n4:PODEM algo\n5:D-Algorithm\n6:Placeholder algo\n7: Reset ATG')
+def comms():
+    print(cl.Fore.BLUE + 'Enter an option:\n'
+          '0: generate data structures\n'
+          '1: perform fault collapsing and create fault classes\n'
+          '2:Display found fault classes\n'
+          '3:Simulate the circuit\n'
+          '4:PODEM algo\n'
+          '5:D-Algorithm\n'
+          '6:Placeholder algo\n'
+          '7: Reset ATG\n'
+          '\'help\': Display console options again')
+
+
+# While loop continously scans user input
+comms()
+while (True):
+    option = input(cl.Fore.YELLOW + 'ATG.py> ' + cl.Style.RESET_ALL)
+
+    match option:
+        case '0':
+            gen_struct()
+            comms()
+        case '1':
+            gen_struct()
+        case '2':
+            gen_struct()
+        case '3':
+            gen_struct()
+        case '4':
+            gen_struct()
+        case '5':
+            gen_struct()
+        case '6':
+            gen_struct()
+        case '7':
+            gen_struct()
+        case 'help':
+            comms()
+        case _:
+            print(cl.Fore.RED + '   UNKOWNN COMMAND: "' + option + '"')
