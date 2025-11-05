970ce71621bcb945f1b0e7c13da79e636f1feb78
GravityGravity <154651849+GravityGravity@users.noreply.github.com>
2025-11-05 15:08:55 -0500

    Created B_logic for logic handling     B_logic.py contains al gate truth table logic,     gate operation functions, and Enum types like gate_type and Logic_type

--- Circ.py
+++ Circ.py
@@ -1,24 +1,7 @@
+# FILE: Circ.py (Circuit structure)
 
 from enum import Enum, auto
-
-
-class logic(Enum):
-    ZERO = 0
-    ONE = 1
-    D = (0, 1)  # D
-    D_ = (1, 0)  # Not D
-    X = (1, 0)
-
-
-class gate_logic_types(Enum):  # (controlling value, inversion signal)
-    AND = (0, 0)
-    OR = (1, 0)
-    NOT = (logic.X, 1)
-    NAND = (0, 1)
-    NOR = (1, 1)
-    XOR = 'idk'  # fix this
-
-
+from B_logic import L, g_types
 class Circuit:
 
     def __init__(self):
@@ -30,8 +13,9 @@ class Circuit:
 
         fault_universe = []
         fault_list = []
+        coll_faults = []
 
-    def add_gate(self, gid: str, gtype: gate_logic_types, g_inputs: list, g_output):
+    def add_gate(self, gid: str, gtype: g_types, g_inputs: list, g_output):
         g = gate(gid, gtype, g_inputs, g_output)
         self.gates[gid] = g
 
@@ -50,8 +34,14 @@ class line:
 class gate:
 
     # Class Attributes
-    def __init__(self, gid: str, gtype: gate_logic_types, g_inputs: list, g_output: line):
+    def __init__(self, gid: str, gtype: g_types, g_inputs: list, g_output: line):
         self.gate_id = gid
         self.type = gtype
         self.gate_line_inputs = g_inputs
         self.gate_line_output = g_output
+
+    
+
+    
+    
+
