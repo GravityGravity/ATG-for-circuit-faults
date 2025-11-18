9ab11ebc980ed23e38fb0c1e5e5d2a0f59bf8b57
Gravity <isaakhern@outlook.com>
2025-11-18 17:24:24 -0500

    Additions to Circuit class functions...     Changed specific attributes like primary_in primary_out and fanout to a set for instant lookups
        line class: Changed nxt list from list to a set for instant lookups
                    Added a change_fanout function to set the bool of fanout
    
        add_gate(): Added fanout detection for input lines and added errors checks for duplicate output lines and additional checks
                    if input lines already exisited.
    
        Added functionality to (print_circuit) to print all gates, lines, and circuit related info

--- Circ.py
+++ Circ.py
@@ -6,66 +6,132 @@
 from enum import Enum, auto
 from B_logic import *
 import colorama as cl
+cl.init(autoreset=True)
 
 
 class Circuit():
 
-    def __init__(self):
-        self.Primary_in = []
-        self.Primary_out = []
+    def __init__(self, name: str):
+        self.circuit_name: str = name
+        self.Primary_in: set[str] = set()
+        self.Primary_out: set[str] = set()
 
-        self.gates = {}  # 'key' : 'value'
-        self.lines = {}
-        self.fanouts = {}
+        self.gates: dict[str, gate] = {}  # 'key' : 'value'
+        self.lines: dict[str, line] = {}
+        self.fanouts: set[str] = set()
 
-        fault_universe = []
-        # fault_list = []
-        # coll_faults = []
+        self.fault_universe = []
+        # self.fault_list = [] # Place Holders
+        # self.coll_faults = []
 
     def add_gate(self, gid: str, gtype: g_types, g_inputs: list, g_output: str):
         for inp in g_inputs:
-            if not self.lines.get(inp):
-                print(
-                    cl.Fore.RED + '    ERROR(Circ.py): add_gate() input line does not exist in line\{} dict')
 
-        if self.lines.get(g_output):
-            temp_line = self.lines[g_output]
-            temp_line.is_fanout = True
+            inp_line = self.lines.get(inp)
+            if not inp_line:
+                msg = f'ERROR(Circ.py): add_gate() input line does not exist in circuit lines \\{inp} dict EXITING>>>...'
+                print(cl.Fore.RED + '    ' + msg)
+                raise KeyError(msg)
+
+            if len(inp_line.nxt) > 1:
+                if not inp_line.is_fanout:
+                    inp_line.fan_out_change(True)
+                if not inp_line.line_id in self.fanouts:
+                    self.fanouts.add(inp_line.line_id)
+
+        if not self.lines.get(g_output):
+            temp_line = line(g_output)
+            self.lines[g_output] = temp_line
+        else:
+            msg = f'ERROR(Circ.py): add_gate() output already exists \\{g_output} dict EXITING>>>...'
+            print(cl.Fore.RED + '    ' + msg)
+            raise KeyError(msg)
 
         g = gate(gid, gtype, g_inputs, g_output)
+        print(
+            f'     {cl.Fore.GREEN}__Created New Gate {cl.Fore.WHITE}{g.gate_id} ')
         self.gates[gid] = g
 
     def add_line(self, lid: str):
+        # Check if line already exists
+        if self.lines.get(lid):
+            msg = f'ERROR(Circ.py): add_line() line already exists \\{lid} dict EXITING>>>...'
+            print(cl.Fore.RED + '    ' + msg)
+            raise KeyError(msg)
+
         l = line(lid)
+        print(
+            f'     {cl.Fore.BLUE}_Created New Line {cl.Fore.WHITE}{l.line_id} ')
         self.lines[lid] = l
 
     def get_line(self, s_lid: str):
-        return self.lines[s_lid]
+        return self.lines.get(s_lid)
 
     def get_gate(self, s_gid: str):
-        return self.gates[s_gid]
+        return self.gates.get(s_gid)
 
-    def print_all():
-        return 0
+    def print_circ(self):
+        # Print Circuit Name
+        print(f'========================================')
+        print(f'        {self.circuit_name.upper()}         \n')
+
+        # Print Primary Inputs
+        print(f'{cl.Back.YELLOW}    PRIMARY INPUTS')
+        for PI in self.Primary_in:
+            print(f' {PI} ', end="")
+        print('\n')
+
+        # Print Primary Outputs
+        print(f'{cl.Back.YELLOW}    PRIMARY OUTPUTS')
+        for PO in self.Primary_out:
+            print(f' {PO} ', end="")
+        print('\n')
+
+        # Print Lines
+        print(f'{cl.Back.BLUE}    LINES')
+        for key, value in self.lines.items():
+            print(f' {key} ->', end="")
+            for connected in value.nxt:
+                print(f' [ {connected} ', end="")
+            print('\n')
+        print('\n')
+
+        # print Gates
+        print(f'{cl.Back.GREEN}    GATES')
+        for key, value in self.gates.items():
+            print(f' {key}({value.type.name} ')
+
+            print(f' G_Input: ', end="")
+            for Inp in value.gate_line_inputs:
+                print(f' {Inp} ', end="")
+
+            print(f' G_Output: ', end="")
+            print(f' {value.gate_line_output} ', end="")
+            print('\n')
+        print('\n')
+
+        print('==========FINISHED=========')
 
 
 class line:
 
-    is_fanout = False
-    nxt = []  # The next gate or next fanout this line connects to
-
     def __init__(self, lid: str):
         self.line_id = lid
         self.values = []
+        self.nxt: set[str] = set()
+        self.is_fanout = False
+
+    def fan_out_change(self, fan_out: bool):
+        self.is_fanout = fan_out
 
     def add_nxt(self, item_id: str):
-        self.nxt.append(item_id)
+        self.nxt.add(item_id)
 
 
 class gate:
 
     # Class Attributes
-    def __init__(self, gid: str, gtype: g_types, g_inputs: list, g_output: list):
+    def __init__(self, gid: str, gtype: g_types, g_inputs: list, g_output: str):
         self.gate_id = gid
         self.type = gtype
         self.gate_line_inputs = g_inputs
